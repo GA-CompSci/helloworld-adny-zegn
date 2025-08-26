51ce76030c3c46600fc6de28881920268336381b
adny-zegn <143519714+adny-zegn@users.noreply.github.com>
2025-08-26 13:15:59 +0000

    Tue 8/26 Class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,31 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Different Data Types
+        String myName = "Andy"; // camelCase
+        int x = 57;
+        char c = 'c';
+        double y = 5.5; // Initialization
+        boolean boo; // Declaration
+        boo = false;
+        final String LANGUAGE = "Java"; // const
+
+        System.out.println("Hello, " + myName);
+        System.out.println(5.0f / 4); // float math
+        
+        System.out.println(x - 3);
+        x -= 3; // Also: x = x - 3;
+        System.out.println(x);
+        
+        // Comparisons
+        System.out.println(true == false);
+        System.out.println(!boo);
+
+        //Conditional
+        if (boo) System.out.println("boo is true");
+        else System.out.println("boo is false");
+
+        // Short Circuit
+        int divisor = 0;
+        if (divisor != 0 && 5 / divisor > 1) System.out.println("Uh oh");
     }
 }
